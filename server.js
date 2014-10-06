066c4babcc445f28966951507cf458014046075b
Rishi Ghan <rishi@Rishis-MacBook-Pro.local>
2014-10-06 08:31:40 -0400

    transferring all settings to MEAN stack app

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // server.js
 
     // set up ========================
-    var express  = require('express');
-    var app      = express();                               // create our app w/ express
-    var mongoose = require('mongoose');                     // mongoose for mongodb
-    var morgan = require('morgan');             // log requests to the console (express4)
-    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+    var express  = require('express'),
+    app          = express(),                   // create our app w/ express
+    mongoose     = require('mongoose'),                     // mongoose for mongodb
+    morgan       = require('morgan'),             // log requests to the console (express4)
+    bodyParser   = require('body-parser'),    // pull information from HTML POST (express4)
+    methodOverride = require('method-override'), // simulate DELETE and PUT (express4)
+    port         = process.env.PORT || 3000,
+    request      = require('request'),
+    qs           = require('querystring'),
+    util         = require('util'),
+    QuickBooks   = require('node-quickbooks'),
+    jade         = require('jade')
 
     // configuration =================
 
@@ -22,3 +28,6 @@
     // listen (start app with node server.js) ======================================
     app.listen(8080);
     console.log("App listening on port 8080");
+
+
+
